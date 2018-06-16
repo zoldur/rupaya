462d9554c7da68f1a962c539da417d2b5ebbd5f6
Zoldur <zoldur99@gmail.com>
2018-06-16 03:20:44 -0700

    Get IP

--- rupaya_install.sh
+++ rupaya_install.sh
@@ -13,7 +13,7 @@ COIN_PORT=9020
 RPC_PORT=7020
 
 
-NODEIP=$(curl -s4 icanhazip.com)
+NODEIP=$(curl -s4 api.ipify.org)
 
 
 RED='\033[0;31m'
@@ -140,7 +140,7 @@ function get_ip() {
   declare -a NODE_IPS
   for ips in $(netstat -i | awk '!/Kernel|Iface|lo/ {print $1," "}')
   do
-    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 icanhazip.com))
+    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 api.ipify.org))
   done
 
   if [ ${#NODE_IPS[@]} -gt 1 ]
